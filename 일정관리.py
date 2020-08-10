f1db5778a6120c440f03b3136a03046ccb8fa13a
chaSangguk <68228980+chaSangguk@users.noreply.github.com>
2020-08-10 19:55:39 +0900

    나중을 위해서 더 쉽게 만들어봄

--- 일정관리.py
+++ 일정관리.py
@@ -1,15 +1,19 @@
 import datetime
 Time=[]   #입력받은 시간
 Name = [] #입력받은 이름
-PutTime_=[0 for i in range(2)]
 
 
-PutTime=input("날짜를 입력하세요 : 연 월 일 순으로 입력하세요 : ")
-PutTime=PutTime.split( )#입력 받은 날짜를 띄어쓰기로 분리함.
-PutTime=list(map(int,PutTime))#문자열을 정수 형으로 변환
-PutTime.extend(PutTime_)
-print(PutTime)
-Time.append(datetime.datetime(PutTime[0],PutTime[1],PutTime[2],PutTime[3],PutTime[4]))
+
+#PutTime=input("날짜를 입력하세요 : 연 월 일 순으로 입력하세요 : ")
+#PutTime=PutTime.split( )#입력 받은 날짜를 띄어쓰기로 분리함.
+#PutTime=list(map(int,PutTime))#문자열을 정수 형으로 변환
+#PutTime.extend(PutTime_)
+PutYear=int(input())
+PutMonth=int(input())
+PutDay=int(input())
+PutHour=int(input())
+PutMinute=int(input())
+Time.append(datetime.datetime(PutYear,PutMonth,PutDay,PutHour,PutMinute))
 
 PutName=input()#이름 입력
 Name=[]
