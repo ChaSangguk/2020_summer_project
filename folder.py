db3b41882db2e6ebc088522def913ac09d5a0bd6
chaSangguk <68228980+chaSangguk@users.noreply.github.com>
2020-08-12 21:36:51 +0900

    폴더 생성 및 삭제 파일 만듦, 주석 수정

---
+++ folder.py
@@ -0,0 +1,12 @@
+from os import mkdir,path,remove
+
+def MakeFolder(Path):
+    if path.exists(Path):
+        pass
+    else :
+        mkdir('./'+ Path)
+def DeleteFolder(Path)
+    if path.exists(Path)
+        remove("./"+Path)
+    else:
+        pass
